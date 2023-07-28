b16031c579479a31f8bb71ad34564043ba507102
Larry Chan <larrylaren2@gmail.com>
2023-07-28 15:06:28 +0800

    Added Multiple Linear Regression explainations

--- Multiple_Linear_Regression.py
+++ Multiple_Linear_Regression.py
@@ -1,14 +1,22 @@
 # Explaination of the below pending
-
+# Least squares estimation(Ordinary Least Squares Estimation), estimate model's coefficent
 import numpy as np
 from numpy.linalg import inv
 
 x = np.array([[1, 2, 5], [1, 3, 5], [1, 4, 7]])
 y = np.array([1, 1, 3])
+
+# self.transpose()
 x_transp = x.T
+
+# Matrix product of two arrays.
 x_transp_mult = np.matmul(x_transp, x)
+
+# Inverse Matrix
 x_transp_mult_inv = np.linalg.inv(x_transp_mult)
+
 left_part = np.matmul(x_transp, y)
+
 w = np.matmul(x_transp_mult_inv, left_part)
 
 print(w.round(0))
